94baccfa4d60baa455f30185421d7c9460c83ab2
gbernardino <gabriel.bernardino@upf.edu>
2020-02-28 15:13:36 -0500

    Working on generating a SIP compatible code...
    Data cleaning is important: there are repeated registers (with small differences such as corrected typos), and the codes are not to be trusted...

--- parsingDatabaseUtils.py
+++ parsingDatabaseUtils.py
@@ -19,7 +19,7 @@ cleanWhites = re.compile("[^\S\n]+")
 
 def findInXML(s, et): 
     if isinstance(et, str):
-        raise ValueError('et should be a tree, not a string')
+        et = ET.fromstring(et)
     r =et.find('.//row[@NombreCampo="%s"]' % s)
     if r is not None:
         return r.get('ValorCampo')
@@ -35,7 +35,7 @@ def remove_diacritics(text):
     For example "Héllô" will become "Hello".
     Useful for comparing strings in an accent-insensitive fashion.
     """
-    normalized = unicodedata.normalize("NFKD", text)
+    normalized = unicodedata.normalize("NFKD", str(text))
     return "".join(c for c in normalized if unicodedata.category(c) != "Mn")
 
 def cleanString(text, removeChars = '-:,;', removeWords = []):
@@ -51,9 +51,9 @@ def cleanString(text, removeChars = '-:,;', removeWords = []):
 def removeWords(text, words = []):
     for w in words:
         text = text.replace(' %s ' % w, ' ')
+    return text
         
-        
-def isMaternalRegister(c, registers, raiseError = False):
+def isMaternalRegister(c, registers, idx = None, raiseError = False):
     """
     Gets whether the record is of the mother or the newborn
     
@@ -63,19 +63,19 @@ def isMaternalRegister(c, registers, raiseError = False):
     unknown = 'NA'
     asunto = remove_diacritics(str(c.Asunto)).lower()
     try:
-        if 'neonato' in asunto or 'nacido' in asunto: 
-            return  'newborn'
+        if 'registro del recien nacido' == asunto: 
+            return  'newborn', idx
         elif 'ingreso de urgencias' in asunto or 'notas de ingreso a piso' in asunto:
-            return 'mother'
+            return 'mother', None
         elif c.Padre == c.Padre:
-            return isMaternalRegister(registers.loc[c.Padre], registers, raiseError)
+            return isMaternalRegister(registers.loc[c.Padre], registers, c.Padre, raiseError)
         else:
-            return unknown
+            return unknown, None
     except KeyError as e:
         if raiseError:
             raise(e)
         else:
-            return unknown
+            return unknown, None
 
 
 def parseAntecedentes(t):
@@ -101,3 +101,88 @@ def parseAntecedentes(t):
     # TODO: check that no other word means anything
 
     return antecedentes
+
+def parseDate(s):
+    """
+    Parse dates, in their multiple possibilities
+    """
+    if not isinstance(s, str):
+        return s
+    meses = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE']
+    meses = list(map(lambda s: s[:3].lower(), meses))
+    for i, m in enumerate(meses):
+        s = s.replace(m, '%d' % (i + 1))
+        
+    p = re.findall('([0-9]+)[^0-9]+([0-9]+)[^0-9]+([0-9]+)', s)
+    return p[0]
+
+meses = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE']
+meses = meses + list(map(lambda s: s[:3], meses))
+sep= '\s*[,;:]?\s*'
+separadorFecha = '(?:[\.\\/-]|DE|DEL|\s)'
+date =  '\(?' +  '((?:[0-9]+)'+ sep + separadorFecha + sep + '(?:[0-9]+|%s)'%  '|'.join(meses) + \
+                       sep + separadorFecha + sep + '(?:[0-9]+))' + '\)?' 
+floatParse = '[0-9]*[\.,]?[0-9]+'
+
+"""
+Parse epicrisis
+
+- FUM
+- ECHOS
+- PARACLINICS (TO SOME EXTENT)
+"""
+
+def parseEchographies(t, cleanText = False):
+    """
+    Finds the echos. Returns False if it is stated in the report that there were no echos, or a list  if some echos were found. An empty list is non conclusive
+    """
+    # Cleaning
+    if cleanText:
+        t = cleanString(t).lower()
+        t = removeWords(t, ['a', 'de', 'el', 'que', 'para'])
+
+    if 'no trae eco' in t or 'ni eco' in t:
+        return False
+
+    paraHoyPossibilities = ['hoy', 'para dia hoy']
+    paraHoy = '(?:%s)' % '|'.join(paraHoyPossibilities)
+    semanas = '(?:%s)' % '|'.join(['semanas', 'sem', 'sems'])
+    embarazo = ['embarazo', 'emb', 'embarazo', 'emb', 'reporta embarazo']
+
+    echoLine = '(:?eco[a-z]*\%s|)' % sep +  date +  sep + '(?:%s)' % '|'.join(embarazo) + ' ' + '(' + '(?P<weeksEG>%s)' % floatParse  + ' ' + semanas  + '[,]?)'\
+    +  '( ' + paraHoy + ' ' + floatParse + ' ' + '(:?%s)?' % semanas +  ')?' + '[^\n]*'
+
+    queryEchos = '(eco[a-z]*' + sep +  '(' + echoLine  + '\s*' ')+)'
+    m = re.findall(echoLine, t, re.MULTILINE)
+    return m
+
+noRecuerda = ['no', '\?']
+searchFUM = re.compile('fum'+ sep + '(?::|.)?'+ sep +'(:?' +  date + '|%s)' % ('|'.join(noRecuerda)), flags = re.IGNORECASE)
+def parseGPCA_and_fum(text):
+    """
+    Gets the GPCA and FUM from the Triage or epicrisis.
+    
+    NOTE: some of the cases are incorrect, double check
+    TODO: actually, registro del recien nacido has it as a field.
+    """    
+    allowedStarts = ['7-&gt;', '-', '- antecedentes', 'antecedentes']
+    line = re.findall('^%s(?:%s)?%s' % (sep, '|'.join(allowedStarts), sep) + 'G' + sep + '[0-9]+.*$', text, re.M)
+    if line:
+        f = line[0]
+        G = f(re.findall('g' + sep + '([0-9]+)', line[0]))
+        A = f(re.findall('a' + sep + '([0-9]+)', line[0]))
+        C = f(re.findall('c' + sep + '([0-9]+)', line[0]))
+        P = f(re.findall('p' + sep + '([0-9]+)', line[0])) 
+        parsedGPCA = [G, P, A, C]
+        GPCA_OK = True
+    else:
+        parsedGPCA = []
+        GPCA_OK = False
+    #Prob athere is a better way...
+    parsedFUM = searchFUM.findall(text)
+    #print(parsedFUM[0])
+    return {'fum' : parsedFUM[0][0] if parsedFUM else '',
+            'fum_OK' : len(parsedFUM) > 0 ,
+            'GPCA_OK' : GPCA_OK,
+            'fum_Data' : parsedFUM,
+            'parsedGPCA' : parsedGPCA}
